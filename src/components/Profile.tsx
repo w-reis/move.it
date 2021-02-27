de64d5f4121e04f196d507ef5d9724c7067d5e2b
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-02-27 00:39:11 -0300

    add level up image

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
       <img src="https://github.com/w-reis.png" alt="Wellinton Reis"/>
       <div>
         <strong>Wellinton Reis</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="level"/>
+          Level 1
+        </p>
       </div>
     </div>
   );
