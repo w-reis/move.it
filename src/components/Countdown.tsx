ba83d869bfd01f7faadc7b64f71678730c7101fe
Wellinton Reis <wellinton_fabricio@hotmail.com>
2021-02-27 01:15:08 -0300

    create countdown button

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,7 +2,8 @@ import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
   return (
-    <div className={styles.countdownContainer}>
+   <div>
+      <div className={styles.countdownContainer}>
       <div>
         <span>2</span>
         <span>5</span>
@@ -15,5 +16,10 @@ export function Countdown() {
         <span>0</span>
       </div>
     </div>
+
+    <button type="button" className={styles.countdownButton}>
+      Iniciar um ciclo
+    </button>
+   </div>
   )
 }
